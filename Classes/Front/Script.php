7450c23f3eff0540edac38cdc8cb65bb0e73d6f3
Luc Princen <luc@to-wonder.com>
2015-06-15 17:45:16 +0200

    Button shortcode + Google Event support

--- Classes/Front/Script.php
+++ Classes/Front/Script.php
@@ -162,13 +162,15 @@ class Scripts {
 	public function analytics( $code ){
 
 		$string = "\n<script>
-    			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    			e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    			e.src='//www.google-analytics.com/analytics.js';
-    			r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    			ga('create','".$code."','auto');ga('send','pageview');
-			</script>";
+		 var _gaq = _gaq || [];
+		 _gaq.push(['_setAccount', '".$code."']);
+		 _gaq.push(['_trackPageview']);
+		 (function() {
+		   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		 })();
+		</script>";
 
 		echo preg_replace('/[ \t]+/', ' ', preg_replace('/[\r\n]+/', "\n", $string));
 	}
