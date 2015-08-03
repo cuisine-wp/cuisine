1a177a0c1c938bf5d0ed5318a89e76cf1aa03804
Luc Princen <luc@chefduweb.nl>
2015-08-03 09:45:17 +0200

    Fixes to the Media & Repeater fields

--- cuisine.php
+++ cuisine.php
@@ -3,7 +3,7 @@
  * Plugin Name: Cuisine
  * Plugin URI: http://chefduweb.nl/cuisine
  * Description: A framework for WordPress developers.
- * Version: 1.4.5
+ * Version: 1.4.6
  * Author: Luc Princen
  * Author URI: http://www.chefduweb.nl/
  * License: GPLv3
