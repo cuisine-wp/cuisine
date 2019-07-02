e84eea0800394c3f1ce8e0fa31e566859a7a3171
Luc Princen <hi@lucp.nl>
2019-07-02 11:45:34 +0200

    Added anonymizeIp as a default

--- Classes/Front/Script.php
+++ Classes/Front/Script.php
@@ -265,7 +265,8 @@ class Scripts {
 		 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		 ga('create', '".$code."', 'auto');
+         ga('create', '".$code."', 'auto');
+         ga('set', 'anonymizeIp', true );
 		 ga('send', 'pageview');\n";
 
 		if( apply_filters( 'cuisine_analytics_include_ecommerce', false ) )
